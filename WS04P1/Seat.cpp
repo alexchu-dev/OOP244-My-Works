2bfb20606238152d3d6af686c38d40227aa374c5
Alex Chu <kchu30@myseneca.ca>
2022-10-04 19:47:50 -0400

    just started working...

--- WS04P1/Seat.cpp
+++ WS04P1/Seat.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
 #include "Seat.h"
 namespace sdds {
-   
+   bool Seat::validate(int row, char letter)const {
+      bool seatValid = false;
+      bool jcl = (row >= 1 && row <= 4);
+      bool pey = (row >= 7 && row <= 15);
+      bool ycl = (row >= 20 && row <= 38);
+      if (jcl)
+         seatValid = (letter == 'A' || 'B' || 'E' || 'F');
+      else if (pey || ycl)
+         seatValid = (letter == 'A' || 'B' || 'C' || 'D' || 'E' || 'F');
+      else
+         seatValid = false;
+      return seatValid;
+   }
+   /*bool validate(int row, char letter) {
+      bool seatValid = false;
+      bool jcl = (row >= 1 && row <= 4);
+      bool pey = (row >= 7 && row <= 15);
+      bool ycl = (row >= 20 && row <= 38);
+      if (jcl)
+         if (letter == 'A' || letter == 'B' || letter == 'E' || letter == 'F') { seatValid = true; }
+         else seatValid = false;
+      else if (pey || ycl)
+         if (letter == 'A' || letter == 'B' || letter == 'C' || letter == 'D' || letter == 'E' || letter == 'F') { seatValid = true; }
+         else seatValid = false;
+      else
+         seatValid = false;
+      return seatValid;
+   }*/
 }
--- WS04P1/Seat.h
+++ WS04P1/Seat.h
@@ -2,7 +2,11 @@
 #define SDDS_SEAT_H_
 namespace sdds {
    class Seat {
-
+      char* m_paxName;
+      int m_row;
+      char m_seat;
+   private:
+      bool validate(int row, char letter)const;
    };
 }
 #endif // !SDDS_SEAT_H_
