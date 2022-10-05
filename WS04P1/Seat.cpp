d7f45325979a3605260cc284264fb864daa5710f
Alex Chu <alexchu414@gmail.com>
2022-10-05 00:46:11 -0400

    init to be continue..

--- WS04P1/Seat.cpp
+++ WS04P1/Seat.cpp
@@ -7,19 +7,6 @@ namespace sdds {
       bool pey = (row >= 7 && row <= 15);
       bool ycl = (row >= 20 && row <= 38);
       if (jcl)
-         seatValid = (letter == 'A' || 'B' || 'E' || 'F');
-      else if (pey || ycl)
-         seatValid = (letter == 'A' || 'B' || 'C' || 'D' || 'E' || 'F');
-      else
-         seatValid = false;
-      return seatValid;
-   }
-   /*bool validate(int row, char letter) {
-      bool seatValid = false;
-      bool jcl = (row >= 1 && row <= 4);
-      bool pey = (row >= 7 && row <= 15);
-      bool ycl = (row >= 20 && row <= 38);
-      if (jcl)
          if (letter == 'A' || letter == 'B' || letter == 'E' || letter == 'F') { seatValid = true; }
          else seatValid = false;
       else if (pey || ycl)
@@ -28,5 +15,63 @@ namespace sdds {
       else
          seatValid = false;
       return seatValid;
-   }*/
+   }
+   Seat& Seat::alAndCp(const char* str) {
+      m_paxName = nullptr;
+      return *this;
+   }
+   Seat& Seat::reset() {
+      delete[] m_paxName;
+      m_paxName = nullptr;
+      m_row = 0;
+      m_seat = 0;
+      return *this;
+   }
+   bool Seat::isEmpty() const {
+      return true;
+   }
+   bool Seat::assigned() const {
+      return true;
+   }
+   Seat::Seat() {
+      delete[] m_paxName;
+      m_paxName = nullptr;
+      m_row = 0;
+      m_seat = 0;
+   }
+   Seat::Seat(const char* passengerName) {
+      delete[] m_paxName;
+      m_paxName = nullptr;
+      m_row = 0;
+      m_seat = 0;
+   }
+   Seat::Seat(const char* passengerName, int row, char letter) {
+      delete[] m_paxName;
+      m_paxName = nullptr;
+      m_row = 0;
+      m_seat = 0;
+   }
+   Seat::~Seat() {
+      m_paxName[0] = 0;
+      m_row = 0;
+      m_seat = 0;
+   }
+   Seat& Seat::set(int row, char letter) {
+      return *this;
+   }
+   int Seat::row()const {
+      return 0;
+   }
+   char Seat::letter()const {
+      return 0;
+   }
+   const char* Seat::passenger()const {
+      return nullptr;
+   }
+   std::ostream& Seat::display(std::ostream& coutRef) const {
+      return coutRef;
+   }
+   std::istream& Seat::read(std::istream& cinRef) {
+      return cinRef;
+   }
 }
--- WS04P1/Seat.h
+++ WS04P1/Seat.h
@@ -5,8 +5,22 @@ namespace sdds {
       char* m_paxName;
       int m_row;
       char m_seat;
-   private:
       bool validate(int row, char letter)const;
+      Seat& alAndCp(const char* str);
+   public:
+      Seat& reset();
+      bool isEmpty() const;
+      bool assigned() const;
+      Seat();
+      Seat(const char* passengerName);
+      Seat(const char* passengerName, int row, char letter);
+      ~Seat();
+      Seat& set(int row, char letter);
+      int row()const;
+      char letter()const;
+      const char* passenger()const;
+      std::ostream& display(std::ostream& coutRef = std::cout)const;
+      std::istream& read(std::istream& cinRef = std::cin);
    };
 }
 #endif // !SDDS_SEAT_H_
