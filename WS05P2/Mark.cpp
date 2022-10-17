41ec99ac2b766e9ce211eeb9fa0bd1c18ce984b3
Alex Chu <alexchu414@gmail.com>
2022-10-17 01:45:07 -0400

    Some bugs have to be fixed...

---
+++ WS05P2/Mark.cpp
@@ -0,0 +1,252 @@
+/******************************************************************************
+//                    OOP244NAA - WS05 Part 2 @ Oct 16 2022
+//Full Name  : Alex Chu
+//Student ID#: 153954219
+//Email      : kchu30@myseneca.ca
+//
+//I have done all the coding by myself and only copied the code
+//that my professor provided to complete my workshops and assdignments.
+******************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include "Mark.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+namespace sdds
+{
+   Mark::Mark() {
+      m_value = 0;
+   }
+   Mark::Mark(int value) {
+      if (isValid(value))
+      {
+         m_value = value;
+      }
+      else
+      {
+         m_value = 0;
+      }
+   }
+   bool Mark::isValid(int value) {
+      return value >= 0 && value <= 100;
+   }
+   Mark::operator int() const {
+      return m_value;
+   }
+   Mark::operator double() const {
+      double ret;
+      if (m_value >= 0 && m_value <= 49)
+      {
+         ret = 0.0;
+      }
+      else if (m_value >= 50 && m_value <= 59)
+      {
+         ret = 1.0;
+      }
+      else if (m_value >= 60 && m_value <= 69)
+      {
+         ret = 2.0;
+      }
+      else if (m_value >= 70 && m_value <= 79)
+      {
+         ret = 3.0;
+      }
+      else if (m_value >= 80 && m_value <= 100)
+      {
+         ret = 4.0;
+      }
+      else
+      {
+         ret = 0.0;
+      }
+      return ret;
+   }
+   Mark::operator char() const {
+      char ret;
+      if (m_value >= 0 && m_value <= 49)
+      {
+         ret = 'F';
+      }
+      else if (m_value >= 50 && m_value <= 59)
+      {
+         ret = 'D';
+      }
+      else if (m_value >= 60 && m_value <= 69)
+      {
+         ret = 'C';
+      }
+      else if (m_value >= 70 && m_value <= 79)
+      {
+         ret = 'B';
+      }
+      else if (m_value >= 80 && m_value <= 100)
+      {
+         ret = 'A';
+      }
+      else
+      {
+         ret = 'X';
+      }
+      return ret;
+   }
+   Mark::operator bool() const {
+      return m_value >= 0 && m_value <= 100;
+   }
+   bool Mark::operator>(int value)const {
+      return m_value > value;
+   }
+   bool Mark::operator<(int value)const {
+      return m_value < value;
+   }
+   bool Mark::operator>=(int value)const {
+      return m_value >= value;
+   }
+   bool Mark::operator<= (int value)const {
+      return m_value <= value;
+   }
+   bool Mark::operator== (int value)const {
+      return m_value == value;
+   }
+   bool Mark::operator!= (int value)const {
+      return m_value != value;
+   }
+   Mark& Mark::operator++() {
+      if (isValid(m_value + 1))
+      {
+         m_value++;
+      }
+      return *this;
+   }
+   Mark& Mark::operator++(int) {
+      Mark m = *this;
+      if (isValid(m_value + 1))
+      {
+         ++(*this);
+      }
+      return m;
+   }
+   Mark& Mark::operator--() {
+      if (isValid(m_value - 1))
+      {
+         m_value--;
+      }
+      return *this;
+   }
+   Mark& Mark::operator--(int) {
+      Mark m = *this;
+      if (isValid(m_value - 1))
+      {
+        --(*this);
+      }
+      return m;
+   }
+   bool Mark::operator~()const {
+      return m_value >= 50 && m_value <= 100;
+   }
+   Mark& Mark::operator=(int value) {
+      if (isValid(value))
+      {
+         m_value = value;
+      }
+      else
+      {
+         m_value = 0;
+      }
+      return *this;
+   }
+   Mark& Mark::operator+=(int value) {
+      if (isValid(m_value + value))
+      {
+         m_value += value;
+      }
+      else
+      {
+         m_value = 0;
+      }
+      return *this;
+   }
+   Mark& Mark::operator-=(int value) {
+      if (isValid(m_value - value))
+      {
+         m_value -= value;
+      }
+      else
+      {
+         m_value = 0;
+      }
+      return *this;
+   }
+   Mark& Mark::operator<<(Mark& rhs) {
+      if (isValid(m_value + rhs.m_value))
+      {
+         m_value = m_value + rhs.m_value;
+         rhs.m_value = 0;
+      }
+      else
+      {
+         m_value = 0;
+      }
+      return *this;
+   }
+   Mark& Mark::operator>>(Mark& rhs) {
+      if (isValid(m_value + rhs.m_value))
+      {
+         rhs.m_value = rhs.m_value + m_value;
+         m_value = 0;
+      }
+      else
+      {
+         rhs.m_value = 0;
+      }
+      return *this;
+   }
+   Mark operator+(const Mark& mark, int value) {
+      Mark temp = mark;
+      if (mark.getMark() + value >= 0 && mark.getMark() + value <= 100)
+      {
+         temp += value;
+      }
+      else
+      {
+         temp = 0;
+      }
+      return temp;
+   }
+   Mark operator+(int value, const Mark& mark) {
+      Mark temp = mark;
+      if (mark.getMark() + value >= 0 && mark.getMark() + value <= 100)
+      {
+         temp += value;
+      }
+      else
+      {
+         temp = 0;
+      }
+      return temp;
+   }
+   Mark operator+(const Mark& lhs, const Mark& rhs) {
+      Mark temp = lhs;
+      if (lhs.getMark() + rhs.getMark() >= 0 && lhs.getMark() + rhs.getMark() <= 100)
+      {
+         temp += rhs.getMark();
+      }
+      else
+      {
+         temp = 0;
+      }
+      return temp;
+   }
+   int operator+=(int value, const Mark& mark) {
+      return value + mark.getMark();
+   }
+   int operator+=(const Mark& mark, int value) {
+      return value + mark.getMark();
+   }
+   int operator-=(int value, const Mark& mark) {
+      return value - mark.getMark();
+   }
+   int operator-=(const Mark& mark, int value) {
+      return value - mark.getMark();
+   }
+
+} 
---
+++ WS05P2/Mark.h
@@ -0,0 +1,55 @@
+/******************************************************************************
+//                    OOP244NAA - WS05 Part 2 @ Oct 16 2022
+//Full Name  : Alex Chu
+//Student ID#: 153954219
+//Email      : kchu30@myseneca.ca
+//
+//I have done all the coding by myself and only copied the code
+//that my professor provided to complete my workshops and assdignments.
+******************************************************************************/
+#ifndef SDDS_MARK_H_
+#define SDDS_MARK_H_
+#include <iostream>
+
+namespace sdds
+{
+   class Mark
+   {
+      int m_value = 0; 
+   public:
+      Mark();
+      Mark(int);
+      bool isValid(const int);
+      operator int()const;
+      operator double()const;
+      operator char()const;
+      operator bool()const;
+      bool operator>(int)const;
+      bool operator<(int)const;
+      bool operator>=(int)const;
+      bool operator<=(int)const;
+      bool operator==(int)const;
+      bool operator!=(int)const;
+      Mark& operator++();
+      Mark& operator++(int);
+      Mark& operator--();
+      Mark& operator--(int);
+      bool operator~()const;
+      Mark& operator=(int);
+      Mark& operator+=(int);
+      Mark& operator-=(int);
+      Mark& operator<<(Mark&);
+      Mark& operator>>(Mark&);
+      int getMark()const { return m_value; }
+   };
+   Mark operator+(const Mark&, int);
+   Mark operator+(int, const Mark&);
+   Mark operator+(const Mark&, const Mark&);
+   int operator+=(int, const Mark&);
+   int operator+=(const Mark&, int);
+   int operator-=(int, const Mark&);
+   int operator-=(const Mark&, int);
+}
+ 
+
+#endif // SDDS_MARK_H_
