afd821775a01782d83673d3c8d3250519683fdda
Alex Chu <kchu30@myseneca.ca>
2022-11-04 03:33:50 -0400

    init 2

--- MS1/Menu.cpp
+++ MS1/Menu.cpp
@@ -10,8 +10,22 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+#include <iostream>
 #include "Menu.h"
 using namespace std;
 namespace sdds {
-
+   MenuItem::MenuItem(char* text)
+   {
+      if (text != nullptr) {
+         strncpy(m_text, text, 50);
+         m_text[50] = 0;
+      }
+      else {
+         m_text[0] = 0;
+      }
+   }
+   ostream& MenuItem::display(ostream& coutRef) const
+   {
+      // TODO: insert return statement here
+   }
 }
--- MS1/Menu.h
+++ MS1/Menu.h
@@ -1,12 +1,15 @@
 #ifndef SDDS_MENU_H_
 #define SDDS_MENU_H_
-namespace SDDS {
+#include <iostream>
+namespace sdds {
    class MenuItem {
-
+      char m_text[51];
    public:
+      MenuItem(char* text);
+      std::ostream& display(std::ostream& coutRef = std::cout)const;
+      MenuItem(const MenuItem& src) = delete;
+      MenuItem& operator=(const MenuItem& src) = delete;
       friend class Menu;
    };
 }
-
-
 #endif
