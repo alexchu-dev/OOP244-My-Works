c1887b859ba644e3787e9342acce3f9d9a75aa3d
Alex Chu <kchu30@myseneca.ca>
2022-11-05 00:32:46 -0400

    up until bool cast overload

--- MS1/Menu.cpp
+++ MS1/Menu.cpp
@@ -14,6 +14,10 @@
 #include "Menu.h"
 using namespace std;
 namespace sdds {
+   MenuItem::MenuItem()
+   {
+      setEmpty();
+   }
    MenuItem::MenuItem(char* text)
    {
       if (text != nullptr) {
@@ -21,11 +25,35 @@ namespace sdds {
          m_text[50] = 0;
       }
       else {
-         m_text[0] = 0;
+         setEmpty();
       }
    }
    ostream& MenuItem::display(ostream& coutRef) const
    {
       // TODO: insert return statement here
+   }
+   void MenuItem::setEmpty()
+   {
+      m_text[0] = 0;
+   }
+   Menu::Menu()
+   {
+      setEmpty();
+   }
+   Menu::Menu(char* text, int indentation = 0)
+   {
+      if (text != nullptr) {
+         strncpy(m_title, text, 50);
+         m_title[50] = 0;
+         m_indentation = indentation;
+      }
+      else {
+         setEmpty();
+      }
+   }
+   void Menu::setEmpty()
+   {
+      m_title[0] = 0;
+      m_indentation = 0;
    }
 }
--- MS1/Menu.h
+++ MS1/Menu.h
@@ -1,15 +1,30 @@
 #ifndef SDDS_MENU_H_
 #define SDDS_MENU_H_
+#define MAX_NO_OF_ITEMS 10
 #include <iostream>
 namespace sdds {
    class MenuItem {
       char m_text[51];
    public:
+      MenuItem();
       MenuItem(char* text);
       std::ostream& display(std::ostream& coutRef = std::cout)const;
       MenuItem(const MenuItem& src) = delete;
       MenuItem& operator=(const MenuItem& src) = delete;
+      void setEmpty();
       friend class Menu;
+   };
+   class Menu {
+      char m_title[51];
+      MenuItem m_items[MAX_NO_OF_ITEMS];
+      int m_noOfItems;
+      int m_indentation;
+   public:
+      Menu();
+      Menu(char* text, int indentation = 0);
+      Menu(const Menu& src) = delete;
+      Menu& operator=(const Menu& src) = delete;  
+      void setEmpty();
    };
 }
 #endif
