aa09c6ae44a890a0ff95db4dfa186836f65db67c
Alex Chu <alexchu414@gmail.com>
2022-11-16 23:52:18 -0500

    finished ms3.1, output identical.

---
+++ MS3/ReadWritable.cpp
@@ -0,0 +1,39 @@
+/***********************************************************************
+// ReadWritable Module
+// File  ReadWritable.cpp
+// Version 0.11
+// Date  16 Nov 2022
+// Author   Alex Chu
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+//
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+#include <iostream>
+#include "ReadWritable.h"
+using namespace std;
+namespace sdds {
+   ReadWritable::ReadWritable()
+   {
+      m_flag = false;
+   }
+   bool ReadWritable::isCsv() const
+   {
+      return m_flag;
+   }
+   void ReadWritable::setCsv(bool value)
+   {
+      m_flag = value;
+   }
+   std::ostream& operator<<(std::ostream& ostr, const ReadWritable& RW)
+   {
+      RW.write(ostr);
+      return ostr;
+   }
+   std::istream& operator>>(std::istream& istr, ReadWritable& RW)
+   {
+      RW.read(istr);
+      return istr;
+   }
+}
