6ded00e362ab2df945a8471c1eda42e4639ac136
Alex Chu <61229735+alexchu-dev@users.noreply.github.com>
2023-06-02 23:29:24 -0400

    Update Population.cpp

--- WS02P2/Population.cpp
+++ WS02P2/Population.cpp
@@ -1,13 +1,11 @@
-/*
-*****************************************************************************
+/******************************************************************************
                     OOP244NAA - WS02 Part 2 @ Sep 24 2022
 Full Name  : Alex Chu
 Student ID#: 153954219
 Email      : kchu30@myseneca.ca
 
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assdignments.
-*****************************************************************************
-*/
+******************************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "Population.h"
